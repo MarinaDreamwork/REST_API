3c5de4e25e5b7728c88212b14849a64ef0ec6db8
Marina Trushina <silaevamo@gmail.com>
2022-08-18 08:44:32 +0300

    added env variable

--- db/index.js
+++ db/index.js
@@ -6,7 +6,7 @@ const dbConfig = {
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   host: process.env.DB_HOST,
-  port: 5432
+  port: process.env.DB_PORT
 };
 
 const pool = new Pool(dbConfig);
